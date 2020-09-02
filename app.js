eb8b12a251a7d7dac92d8a486fb3c0acaada7e77
tsungtingdu <tsungtingdu@gmail.com>
2020-09-02 14:29:44 +0800

    setup app.js

--- app.js
+++ app.js
@@ -1,6 +1,30 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const PORT = process.env.PORT || 3000
+const session = require('express-session')
+const routes = require('./routes/index.js')
+
+// cors
+app.use(cors())
+
+// session
+app.use(session({
+  secret: 'akpitdx',
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(express.json())
+
+// locals
+app.use((req, res, next) => {
+  res.locals.user = req.user
+  next()
+})
+
+// routes
+routes(app)
 
 app.listen(PORT, () => {
   console.log(`Node server is listening on port ${PORT}`)
