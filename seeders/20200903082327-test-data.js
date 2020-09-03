2b9a13fd9a005b7c7d818ce434ac4700df975b74
tsungtingdu <tsungtingdu@gmail.com>
2020-09-03 17:25:45 +0800

    create seed data for testing

---
+++ seeders/20200903082327-test-data.js
@@ -0,0 +1,88 @@
+'use strict';
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const { Collaborator, Post, User } = db
+const SEED_USER = [
+  {
+    name: 'user1',
+    email: 'user1@gmail.com',
+    password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+    role: 'user',
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+  {
+    name: 'user2',
+    email: 'user2@gmail.com',
+    password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+    role: 'user',
+    createdAt: new Date(),
+    updatedAt: new Date()
+  },
+  {
+    name: 'user3',
+    email: 'user3@gmail.com',
+    password: bcrypt.hashSync('12345678', bcrypt.genSaltSync(10), null),
+    role: 'user',
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }
+]
+const ROLE = ['viewer', 'collaborator']
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    // create users
+    await queryInterface.bulkInsert('Users', SEED_USER, {})
+
+    // create posts
+    let date = new Date()
+    await queryInterface.bulkInsert('Posts',
+      Array.from({ length: 50 }).map((item, index) => {
+        return {
+          title: `${index}-post`,
+          content: `${index}-Lorem lorem lorem`,
+          status: 'private',
+          createdAt: date,
+          updatedAt: date
+        }
+      }), {})
+
+    // get user id
+    let users = await User.findAll()
+    users = users.map(i => i.id)
+    // get post id
+    let posts = await Post.findAll()
+    posts = posts.map(i => i.id)
+
+    // create owner
+    await queryInterface.bulkInsert('Collaborators',
+      Array.from({ length: posts.length }).map((item, index) => {
+        return {
+          PostId: posts[index],
+          UserId: users[Math.floor(Math.random() * (users.length))],
+          role: 'owner',
+          createdAt: date,
+          updatedAt: date
+        }
+      }), {})
+
+    // create viewer and collaborator
+    return queryInterface.bulkInsert('Collaborators',
+      Array.from({ length: posts.length }).map((item, index) => {
+        return {
+          PostId: posts[index],
+          UserId: users[Math.floor(Math.random() * (users.length))],
+          role: ROLE[Math.floor(Math.random() * (ROLE.length))],
+          createdAt: date,
+          updatedAt: date
+        }
+      }), {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    return queryInterface.bulkDelete('Posts', null, {})
+      .then(() => queryInterface.bulkDelete('Users', null, {})
+        .then(() => queryInterface.bulkDelete('Collaborators', null, {})))
+  }
+};
